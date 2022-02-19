ba75db6deec3f9a35bcc3e7f993c06f2b7dafdbd
Akihiro Kato <4kihiro.kato@gmail.com>
2022-02-19 22:16:19 +0900

    Setting foundations

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts)x?$/]
+      },
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  },
   reactStrictMode: true,
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        gothic: ['Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', 'Meiryo', 'メイリオ' ,'游ゴシック Medium', 'MS PGothic', 'arial', 'helvetica', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
