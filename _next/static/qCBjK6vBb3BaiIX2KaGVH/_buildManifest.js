5bf8d6f75d105a1f4bae6655c5bbcac1feed6561
croe <croe@users.noreply.github.com>
2022-02-20 13:00:37 +0000

    deploy: 669dbb4b7177e7f243e6211ba1cf893649898b7f

---
+++ _next/static/qCBjK6vBb3BaiIX2KaGVH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-49a09f3d0af0ae1a.js","static/chunks/971-a509fb76a924a81e.js","static/chunks/pages/index-ccdf81f9a67b8753.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
