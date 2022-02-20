e93a17668248c4d39480df3a1f34f74b70f5bede
croe <croe@users.noreply.github.com>
2022-02-20 12:25:47 +0000

    deploy: 8c1a1dc9583637d14c06e65a0f5d4007570c05ba

--- _next/static/C3cpYpcrIjqvi-U0B2gfW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/795-5a085a9ffac0b0e8.js","static/chunks/pages/index-f3afbee18270bff9.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
