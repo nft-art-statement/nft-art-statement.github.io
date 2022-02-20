77bbc08c17401a469dace62a3b789abe9e3bf862
wildmouse <wildmouse0813@gmail.com>
2022-02-20 13:43:17 +0900

    Format with prettier

--- pages/index.js
+++ pages/index.js
@@ -1,126 +1,123 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react'
-import { nftContractAddress } from '../config'
-import { ethers } from 'ethers'
-import { abi } from '../utils/abi'
-import tw from 'twin.macro'
-import fetch from 'isomorphic-unfetch'
-import axios from 'axios'
-import { Markdown } from '../components/Markdown'
-import { Button } from '../components/Button'
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { nftContractAddress } from "../config";
+import { ethers } from "ethers";
+import { abi } from "../utils/abi";
+import tw from "twin.macro";
+import fetch from "isomorphic-unfetch";
+import axios from "axios";
+import { Markdown } from "../components/Markdown";
+import { Button } from "../components/Button";
 
-const Container = tw.div`mt-16 w-full tracking-wide leading-relaxed max-w-screen-lg mx-auto px-8 text-justify`
-const SignContainer = tw.div`w-full flex justify-center mt-10 mb-20`
+const Container = tw.div`mt-16 w-full tracking-wide leading-relaxed max-w-screen-lg mx-auto px-8 text-justify`;
+const SignContainer = tw.div`w-full flex justify-center mt-10 mb-20`;
+const SIGNER_AMOUNT_TO_DISPLAY = 10;
+// TODO switch to mainnet
+const infura = "https://rinkeby.infura.io/v3/3ee178f96e63405181f410fe91325b3b";
+const provider = new ethers.providers.JsonRpcProvider(infura);
 
-const Home = ({data}) => {
-  const [mintedNFT, setMintedNFT] = useState(null)
-  const [miningStatus, setMiningStatus] = useState(null)
-  const [loadingState, setLoadingState] = useState(0)
-  const [txError, setTxError] = useState(null)
-  const [currentAccount, setCurrentAccount] = useState('')
-  const [correctNetwork, setCorrectNetwork] = useState(false)
+const Home = ({ data }) => {
+  const [mintedNFT, setMintedNFT] = useState(null);
+  const [miningStatus, setMiningStatus] = useState(null);
+  const [loadingState, setLoadingState] = useState(0);
+  const [txError, setTxError] = useState(null);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [correctNetwork, setCorrectNetwork] = useState(false);
   const [signerList, setSignerList] = useState([]);
   const [signerListWithENS, setSignerListWithENS] = useState([]);
-  const SIGNER_AMOUNT_TO_DISPLAY = 10;
+
+  // TODO put as a demo. Remove this one.
+  // console.log(signerList, signerListWithENS);
 
   // Checks if wallet is connected
   const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window
+    const { ethereum } = window;
     if (ethereum) {
-      console.log('Got the ethereum obejct: ', ethereum)
+      console.log("Got the ethereum obejct: ", ethereum);
     } else {
-      console.log('No Wallet found. Connect Wallet')
+      console.log("No Wallet found. Connect Wallet");
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' })
+    const accounts = await provider.send("eth_accounts");
 
     if (accounts.length !== 0) {
-      console.log('Found authorized Account: ', accounts[0])
-      setCurrentAccount(accounts[0])
+      console.log("Found authorized Account: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
     } else {
-      console.log('No authorized account found')
+      console.log("No authorized account found");
     }
-  }
+  };
 
   // Calls Metamask to connect wallet on clicking Connect Wallet button
   const connectWallet = async () => {
     try {
-      const { ethereum } = window
+      let chainId = await provider.send("eth_chainId");
+      console.log("Connected to chain:" + chainId);
 
-      if (!ethereum) {
-        console.log('Metamask not detected')
-        return
-      }
-      let chainId = await ethereum.request({ method: 'eth_chainId' })
-      console.log('Connected to chain:' + chainId)
+      const rinkebyChainId = "0x4";
 
-      const rinkebyChainId = '0x4'
-
-      const devChainId = 1337
-      const localhostChainId = `0x${Number(devChainId).toString(16)}`
+      const devChainId = 1337;
+      const localhostChainId = `0x${Number(devChainId).toString(16)}`;
 
       if (chainId !== rinkebyChainId && chainId !== localhostChainId) {
-        alert('You are not connected to the Rinkeby Testnet!')
-        return
+        alert("You are not connected to the Rinkeby Testnet!");
+        return;
       }
 
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      const accounts = await provider.send({
+        method: "eth_requestAccounts",
+        params: [],
+      });
 
-      console.log('Found account', accounts[0])
-      setCurrentAccount(accounts[0])
+      console.log("Found account", accounts[0]);
+      setCurrentAccount(accounts[0]);
     } catch (error) {
-      console.log('Error connecting to metamask', error)
+      console.log("Error connecting to metamask", error);
     }
-  }
+  };
 
   // Checks if wallet is connected to the correct network
   const checkCorrectNetwork = async () => {
-    const { ethereum } = window
-    let chainId = await ethereum.request({ method: 'eth_chainId' })
-    console.log('Connected to chain:' + chainId)
+    const { ethereum } = window;
+    let chainId = await ethereum.request({ method: "eth_chainId" });
+    console.log("Connected to chain:" + chainId);
 
-    const rinkebyChainId = '0x4'
+    const rinkebyChainId = "0x4";
 
-    const devChainId = 1337
-    const localhostChainId = `0x${Number(devChainId).toString(16)}`
+    const devChainId = 1337;
+    const localhostChainId = `0x${Number(devChainId).toString(16)}`;
 
     if (chainId !== rinkebyChainId && chainId !== localhostChainId) {
-      setCorrectNetwork(false)
+      setCorrectNetwork(false);
     } else {
-      setCorrectNetwork(true)
+      setCorrectNetwork(true);
     }
-  }
+  };
 
   useEffect(() => {
-    checkIfWalletIsConnected()
-    checkCorrectNetwork()
-  }, [])
+    checkIfWalletIsConnected();
+    checkCorrectNetwork();
+  }, []);
 
   // Fetch all signer address list by Sign events
   useEffect(() => {
     const f = async () => {
       try {
-        const { ethereum } = window;
+        const nftContract = new ethers.Contract(
+          nftContractAddress,
+          abi,
+          provider
+        );
 
-        if (ethereum) {
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const nftContract = new ethers.Contract(
-            nftContractAddress,
-            abi,
-            provider
-          );
-
-          const signEvents = await nftContract.queryFilter("Sign");
-          const signerAddresses = signEvents
-            .sort((a, b) => b.blockNumber - a.blockNumber)
-            .map((e) => e.args.signer);
-          setSignerList(signerAddresses);
-          setSignerListWithENS(
-            signerAddresses.slice(0, SIGNER_AMOUNT_TO_DISPLAY)
-          );
-        } else {
-          console.log("Ethereum object doesn't exist!");
-        }
+        const signEvents = await nftContract.queryFilter("Sign");
+        const signerAddresses = signEvents
+          .sort((a, b) => b.blockNumber - a.blockNumber)
+          .map((e) => e.args.signer);
+        setSignerList(signerAddresses);
+        setSignerListWithENS(
+          signerAddresses.slice(0, SIGNER_AMOUNT_TO_DISPLAY)
+        );
+        // console.log(signerAddresses);
       } catch (error) {
         console.log("Error minting character", error);
         setTxError(error.message);
@@ -131,7 +128,6 @@ const Home = ({data}) => {
 
   // map ens name to signer addresses of first specified amount
   useEffect(() => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
     const f = async () => {
       const nameList = await Promise.all(
         signerList.slice(0, SIGNER_AMOUNT_TO_DISPLAY).map(async (signer) => {
@@ -147,85 +143,66 @@ const Home = ({data}) => {
   // Creates transaction to mint NFT on clicking Mint Character button
   const mintCharacter = async () => {
     try {
-      const { ethereum } = window
+      const signer = provider.getSigner();
+      const nftContract = new ethers.Contract(nftContractAddress, abi, signer);
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const nftContract = new ethers.Contract(
-          nftContractAddress,
-          abi,
-          signer
-        )
+      let nftTx = await nftContract.signToStatementAndMintBadge();
+      console.log("Mining....", nftTx.hash);
+      setMiningStatus(0);
 
-        let nftTx = await nftContract.signToStatementAndMintBadge()
-        console.log('Mining....', nftTx.hash)
-        setMiningStatus(0)
+      let tx = await nftTx.wait();
+      setLoadingState(1);
+      console.log("Mined!", tx);
+      let event = tx.events[0];
+      let value = event.args[2];
+      let tokenId = value.toNumber();
 
-        let tx = await nftTx.wait()
-        setLoadingState(1)
-        console.log('Mined!', tx)
-        let event = tx.events[0]
-        let value = event.args[2]
-        let tokenId = value.toNumber()
+      console.log(
+        `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTx.hash}`
+      );
 
-        console.log(
-          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTx.hash}`
-        )
-
-        getMintedNFT(tokenId)
-      } else {
-        console.log("Ethereum object doesn't exist!")
-      }
+      getMintedNFT(tokenId);
     } catch (error) {
-      console.log('Error minting character', error)
-      setTxError(error.message)
+      console.log("Error minting character", error);
+      setTxError(error.message);
     }
-  }
+  };
 
   // Gets the minted NFT data
   const getMintedNFT = async (tokenId) => {
     try {
-      const { ethereum } = window
+      const signer = provider.getSigner();
+      const nftContract = new ethers.Contract(nftContractAddress, abi, signer);
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const nftContract = new ethers.Contract(
-          nftContractAddress,
-          abi,
-          signer
-        )
+      let tokenUri = await nftContract.tokenURI(tokenId);
+      let data = await axios.get(tokenUri);
+      let meta = data.data;
 
-        let tokenUri = await nftContract.tokenURI(tokenId)
-        let data = await axios.get(tokenUri)
-        let meta = data.data
-
-        setMiningStatus(1)
-        setMintedNFT(meta.image)
-      } else {
-        console.log("Ethereum object doesn't exist!")
-      }
+      setMiningStatus(1);
+      setMintedNFT(meta.image);
     } catch (error) {
-      console.log(error)
-      setTxError(error.message)
+      console.log(error);
+      setTxError(error.message);
     }
-  }
+  };
+
   return (
     <>
       <Head>
-        <title>〈NFTアート〉への共同ステートメント | Community Statement on “NFT art”</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <title>
+          〈NFTアート〉への共同ステートメント | Community Statement on “NFT art”
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Container>
         <Markdown contents={data} />
         <SignContainer>
-          {currentAccount === '' ? (
+          {currentAccount === "" ? (
             <Button onClick={connectWallet}>Connect Wallet</Button>
           ) : correctNetwork ? (
             <Button onClick={mintCharacter}>Sign Statement</Button>
           ) : (
-            <div tw='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
+            <div tw="flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3">
               <div>----------------------------------------</div>
               <div>Please connect to the Rinkeby Testnet</div>
               <div>and reload the page</div>
@@ -235,17 +212,19 @@ const Home = ({data}) => {
         </SignContainer>
       </Container>
     </>
-  )
-}
+  );
+};
 
 export const getStaticProps = async (context) => {
-  const res = await fetch('https://raw.githubusercontent.com/nft-art-statement/statement/main/statement.md')
-  const text = await res.text()
+  const res = await fetch(
+    "https://raw.githubusercontent.com/nft-art-statement/statement/main/statement.md"
+  );
+  const text = await res.text();
   return {
     props: {
-      data: text
-    }
-  }
-}
+      data: text,
+    },
+  };
+};
 
-export default Home
+export default Home;
