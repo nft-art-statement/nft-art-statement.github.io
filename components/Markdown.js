bd4c3124df425c9fbe5d803b0d2675c23b2960e0
Akihiro Kato <4kihiro.kato@gmail.com>
2022-02-20 21:53:29 +0900

    Fixed size

--- components/Markdown.js
+++ components/Markdown.js
@@ -5,15 +5,16 @@ const Container = styled.div(() => [
   `h1 {
     font-size: 2.5rem;
     font-weight: bold;
-    @media screen and () {
-      
+    @media screen and (max-width: 768px) {
+      font-size: 2rem;
     }
   }
   h2 {
     font-size: 2rem;
     margin-top: 2rem;
-    @media screen and () {
-      
+    @media screen and (max-width: 768px) {
+      font-size: 1.3rem;
+      margin-top: 1.3rem;
     }
   }
   p {
@@ -21,8 +22,11 @@ const Container = styled.div(() => [
     margin-top: 2rem;
     padding-left: 2rem;
     padding-right: 2rem;
-    @media screen and () {
-      
+    @media screen and (max-width: 768px) {
+      font-size: 1rem;
+      margin-top: 1rem;
+      padding-left: 1rem;
+      padding-right: 1rem;
     }
   }
   ul {
@@ -34,10 +38,20 @@ const Container = styled.div(() => [
     li {
       margin-bottom: 1rem;
     }
+    @media screen and (max-width: 768px) {
+      font-size: 1rem;
+      margin-top: 1rem;
+      padding-left: 1.5rem;
+      padding-right: 1rem;
+    }
   }
   hr {
     margin-top: 3rem;
     margin-bottom: 3rem;
+    @media screen and (max-width: 768px) {
+      margin-top: 1.5rem;
+      margin-bottom: 1.5rem;
+    }
   }
   `
 ])
